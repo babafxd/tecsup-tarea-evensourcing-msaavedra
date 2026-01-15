5dcf134d39f2544d956d8fc8f93fb95381c7b712
Marco Saavedra <msaavedra.4595@gmail.com>
2026-01-14 22:15:23 -0500

    Feat: lectura de eventos

--- arq_m3_t2_eda_intro_lms/src/main/java/com/tecsup/lms/admin/infraestructure/web/controller/EDAController.java
+++ arq_m3_t2_eda_intro_lms/src/main/java/com/tecsup/lms/admin/infraestructure/web/controller/EDAController.java
@@ -6,6 +6,7 @@ import com.tecsup.lms.shared.infrastructure.eventsourcing.MemoryEventStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,17 @@ public class EDAController {
 
     private final MemoryEventStore eventStore;
     @GetMapping
-    public ResponseEntity<EDAResponse> getEvents() {
+    public ResponseEntity<EDAResponse> getAllEvents() {
         EDAResponse response = new EDAResponse();
         response.setEvents(eventStore.getAllEvents());
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EDAResponse> getEvents(@PathVariable String id) {
+        EDAResponse response = new EDAResponse();
+        response.setEvents(eventStore.getEvents(id));
         return ResponseEntity.ok(response);
     }
 
